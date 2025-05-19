c8650cfc271be1319a4ebc1365601dee94479bd1
Adnane BC <adnane.bachchar@gmail.com>
2025-05-19 09:04:29 +0100

    i Love C

---
+++ 7-multi_c.js
@@ -0,0 +1,7 @@
+const { argv } = require("node:process");
+
+if (argv[2]) {
+  for (let i = 0; i < argv[2]; i++) {
+    console.log("C is fun");
+  }
+} else console.log("Missing number of occurrences");
